a0e0165195c40971542750db017775c7b67e277c
dhruvthakkar07 <142619847+dhruvthakkar07@users.noreply.github.com>
2023-08-29 10:45:25 +0530

    dhruv thakkar

---
+++ m+n.java
@@ -0,0 +1,34 @@
+public class subract
+{
+public static void main()
+{
+int arrM[][] = {
+{-1, 0, 2},
+{-3, -1, 6},
+{4, 3, -1}
+};
+int arrSum[][] = {
+{-6, 9, 4},
+{4, 5, 0},
+{1, -2, -3}
+};
+int arrN[][] = new int[3][3];
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+arrN[i][j] = arrSum[i][j] - arrM[i][j];
+}
+}
+System.out.println("Array N:");
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+System.out.print(arrN[i][j]);
+System.out.print(' ');
+}
+System.out.println();
+}
+}
+}
